df86c312a72cd2558c1a97ed98eb846e2870cdcd
Riccardo M. Cefala <riccardo.cefala@container-solutions.com>
2019-03-22 17:36:42 +0000

    wipe code generated with older operator-sdk

--- pkg/apis/externalsecret-operator/v1alpha1/doc.go
+++
@@ -1,3 +0,0 @@
-// +k8s:deepcopy-gen=package
-// +groupName=externalsecret-operator.container-solutions.com
-package v1alpha1
--- pkg/secrets/backend.go
+++
@@ -1,7 +0,0 @@
-package secrets
-
-// SecretsBackend is an interface to a Secret Store
-type SecretsBackend interface {
-	Init(...interface{}) error
-	Get(string) (string, error)
-}
