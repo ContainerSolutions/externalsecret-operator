892fb83f3e393c17506160dbc261b7d2bc82353f
Frank Scholten <frank.scholten@container-solutions.com>
2019-05-29 16:22:21 +0200

    Refactored main utility to integration test.

---
+++ pkg/integration/onepassword_integration_test.go
@@ -0,0 +1,33 @@
+package integration
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ContainerSolutions/externalsecret-operator/pkg/secrets"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestOnePasswordBackend(t *testing.T) {
+	Convey("Given an initialized OnePasswordBackend", t, func() {
+		vault := "Personal"
+		key := "testkey"
+		expectedValue := "testvalue"
+
+		client := secrets.OnePasswordCliClient{}
+		backend := secrets.NewOnePasswordBackend(vault, client)
+
+		err := backend.Init(vault)
+		if err != nil {
+			fmt.Println("Init: " + err.Error())
+		}
+
+		Convey("When retrieving a secret", func() {
+			actualValue, err := backend.Get(key)
+			Convey("Then no error is returned", func() {
+				So(err, ShouldBeNil)
+				So(actualValue, ShouldEqual, expectedValue)
+			})
+		})
+	})
+}
