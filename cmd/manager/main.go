7126e3d2b48c56cd606ca7c595b22c0e6923f672
Riccardo M. Cefala <riccardo.cefala@container-solutions.com>
2019-06-16 17:58:14 +0200

    log version and bump

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/ContainerSolutions/externalsecret-operator/pkg/apis"
 	"github.com/ContainerSolutions/externalsecret-operator/pkg/controller"
 	"github.com/ContainerSolutions/externalsecret-operator/secrets/backend"
+	"github.com/ContainerSolutions/externalsecret-operator/version"
 
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
@@ -37,6 +38,7 @@ func printVersion() {
 	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
 	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH))
 	log.Info(fmt.Sprintf("Version of operator-sdk: %v", sdkVersion.Version))
+	log.Info(fmt.Sprintf("External Secret Operator Version: %v", version.Version))
 }
 
 func main() {
