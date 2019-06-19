7d57c02d898ec8aa61d70ad120845e736ebe4faf
Frank Scholten <frank.scholten@container-solutions.com>
2019-06-19 13:29:43 +0200

    #55 Documented prerequisites for 1Password. Added screenshots.

--- deploy/source-onepassword-secrets.sh
+++ deploy/source-onepassword-secrets.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-OP_ITEM_JSON=$(op get item "External Secret Operator - 1Password")
+OP_ITEM_JSON=$(op get item "1Password - External Secret Operator")
 
 export OP_DOMAIN=$(echo $OP_ITEM_JSON | jq -r '.overview.url')
 export OP_EMAIL=$(echo $OP_ITEM_JSON | jq -r '.details.sections[0].fields[] | select(.t == "email").v')
 export OP_SECRET_KEY=$(echo $OP_ITEM_JSON | jq -r '.details.fields[] | select(.designation == "username").value')
 export OP_MASTER_PASSWORD=$(echo $OP_ITEM_JSON | jq -r '.details.fields[] | select(.designation == "password").value')
-export OP_VAULT="Personal"
+export OP_VAULT="test vault one"
 export BACKEND="onepassword"
 
-export OPERATOR_CONFIG="{ \"Name\": \"onepassword\", \"Type\": \"onepassword\", \"Parameters\": {\"domain\": \"${OP_DOMAIN}\", \"email\": \"${OP_EMAIL}\", \"secretKey\": \"${OP_SECRET_KEY}\", \"masterPassword\": \"${OP_MASTER_PASSWORD}\", \"vault\": \"Personal\" }}"
+export OPERATOR_CONFIG="{ \"Name\": \"onepassword\", \"Type\": \"onepassword\", \"Parameters\": {\"domain\": \"${OP_DOMAIN}\", \"email\": \"${OP_EMAIL}\", \"secretKey\": \"${OP_SECRET_KEY}\", \"masterPassword\": \"${OP_MASTER_PASSWORD}\", \"vault\": \"${OP_VAULT}\" }}"
