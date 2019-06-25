7eb5fa083f99d6685e3cfb40b5ae70ce8c9707be
Riccardo M. Cefala <riccardo.cefala@container-solutions.com>
2019-06-25 16:29:58 +0100

    add some comments to 1Password client

--- secrets/onepassword/client.go
+++ secrets/onepassword/client.go
@@ -7,15 +7,18 @@ import (
 	"github.com/pkg/errors"
 )
 
+// Client represents a 1Password Client
 type Client interface {
 	SignIn(domain string, email string, secretKey string, masterPassword string) error
 	Get(vault string, key string) (string, error)
 }
 
+// OP is a 1Password Client
 type OP struct {
 	OP *op.Client
 }
 
+// SignIn sings into a 1Password account corresponding to the parameters
 func (client *OP) SignIn(domain string, email string, secretKey string, masterPassword string) error {
 	op, err := op.NewClient("/usr/local/bin/op", domain, email, masterPassword, secretKey)
 	if err != nil {
@@ -27,6 +30,7 @@ func (client *OP) SignIn(domain string, email string, secretKey string, masterPa
 	return nil
 }
 
+// Get returns the value of the secret with 'key' or error
 func (client *OP) Get(vault string, key string) (string, error) {
 	itemMap, err := client.OP.GetItem(op.VaultName(vault), op.ItemName(key))
 	if itemMap == nil {
