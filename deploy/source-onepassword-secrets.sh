1cbe7f668d7760fc2a9d4a78d97f9b27eb32d1a0
Frank Scholten <frank.scholten@container-solutions.com>
2019-06-28 13:55:18 +0200

    #65 Fixed bug in user_setup script. Source script now parses domain properly.

--- deploy/source-onepassword-secrets.sh
+++ deploy/source-onepassword-secrets.sh
@@ -5,7 +5,7 @@ ITEM_VAULT="${ITEM_VAULT:-Personal}"
 
 OP_ITEM_JSON=$(op get item "${ITEM_NAME}" --vault=${ITEM_VAULT})
 
-export OP_DOMAIN=$(echo $OP_ITEM_JSON | jq -r '.overview.url')
+export OP_DOMAIN=$(echo $OP_ITEM_JSON | jq -r '.overview.url' | sed 's/https:\/\/\(.*\).1password.com/\1/')
 export OP_EMAIL=$(echo $OP_ITEM_JSON | jq -r '.details.sections[0].fields[] | select(.t == "email").v')
 export OP_SECRET_KEY=$(echo $OP_ITEM_JSON | jq -r '.details.fields[] | select(.designation == "username").value')
 export OP_MASTER_PASSWORD=$(echo $OP_ITEM_JSON | jq -r '.details.fields[] | select(.designation == "password").value')
