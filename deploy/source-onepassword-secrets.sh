1ac7636a1c8b3d83c268e00b5ab5570e2df00cdc
Frank Scholten <frank.scholten@container-solutions.com>
2019-06-28 12:39:23 +0200

    #65 Introduced naming convention for 1Password operator account.

--- deploy/source-onepassword-secrets.sh
+++ deploy/source-onepassword-secrets.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-OP_ITEM_JSON=$(op get item "1Password - External Secret Operator")
+ITEM_NAME="${ITEM_NAME:-External Secret Operator minikube}"
+ITEM_VAULT="${ITEM_VAULT:-Personal}"
+
+OP_ITEM_JSON=$(op get item "${ITEM_NAME}" --vault=${ITEM_VAULT})
 
 export OP_DOMAIN=$(echo $OP_ITEM_JSON | jq -r '.overview.url')
 export OP_EMAIL=$(echo $OP_ITEM_JSON | jq -r '.details.sections[0].fields[] | select(.t == "email").v')
