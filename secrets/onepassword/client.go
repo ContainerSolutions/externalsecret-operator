53453802852a4a7cac512019f965a6ca789f0dd0
Frank Scholten <frank.scholten@container-solutions.com>
2019-09-26 15:06:24 +0200

    Renamed OP field to CLI to make it clear this is the wrapper around the op cli.

--- secrets/onepassword/client.go
+++ secrets/onepassword/client.go
@@ -15,24 +15,24 @@ type Client interface {
 
 // OP is a 1Password Client
 type OP struct {
-	OP *op.Client
+	CLI *op.Client
 }
 
-// SignIn sings into a 1Password account corresponding to the parameters
+// Signs into a 1Password account via the op cli
 func (client *OP) SignIn(domain string, email string, secretKey string, masterPassword string) error {
-	op, err := op.NewClient("/usr/local/bin/op", domain, email, masterPassword, secretKey)
+	cli, err := op.NewClient("/usr/local/bin/op", domain, email, masterPassword, secretKey)
 	if err != nil {
 		return errors.Wrap(err, "op signin failed")
 	}
 
-	client.OP = op
+	client.CLI = cli
 
 	return nil
 }
 
 // Get returns the value of the secret with 'key' or error
 func (client *OP) Get(vault string, key string) (string, error) {
-	itemMap, err := client.OP.GetItem(op.VaultName(vault), op.ItemName(key))
+	itemMap, err := client.CLI.GetItem(op.VaultName(vault), op.ItemName(key))
 	if itemMap == nil {
 		return "", fmt.Errorf("could not retrieve 1password item '%s'", key)
 	}
