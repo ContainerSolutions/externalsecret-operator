8f08914eb453b62e71a389c983b2d588b7b0c84f
Frank Scholten <frank.scholten@container-solutions.com>
2019-09-27 12:54:56 +0200

    Implemented SignIn method of fake Cli and refactored field names.

--- secrets/onepassword/fakecli.go
+++ secrets/onepassword/fakecli.go
@@ -5,17 +5,21 @@ import (
 )
 
 type FakeCli struct {
-	Key   string
-	Value string
+	ItemName   string
+	ItemValue string
 }
 
 func (f *FakeCli) GetItem(vault op.VaultName, item op.ItemName) (op.ItemMap, error) {
 	im := make(op.ItemMap)
 
 	fm := make(op.FieldMap)
-	fm[op.FieldName(f.Key)] = op.FieldValue(f.Value)
+	fm[op.FieldName(f.ItemName)] = op.FieldValue(f.ItemValue)
 
-	im[op.SectionName("External Secret Operator")] = fm	
+	im[op.SectionName("External Secret Operator")] = fm
 
 	return im, nil
+}
+
+func (f *FakeCli) SignIn(domain string, email string, secretKey string, masterPassword string) error {
+	return nil
 }
--- secrets/onepassword/fakecli_test.go
+++ secrets/onepassword/fakecli_test.go
@@ -9,21 +9,42 @@ import (
 
 func TestGetItem(t *testing.T) {
 	vault := "Shared"
-	secretKey := "secretKey"
-	secretValue := "secretValue"
+	itemName := "itemName"
+	itemValue := "itemValue"
 	sectionName := "External Secret Operator"
 
 	cli := &FakeCli{
-		Key:   secretKey,
-		Value: secretValue,
+		ItemName:   itemName,
+		ItemValue: itemValue,
 	}
 
-	itemMap, _ := cli.GetItem(op.VaultName(vault), op.ItemName(secretKey))
+	itemMap, _ := cli.GetItem(op.VaultName(vault), op.ItemName(itemName))
 
-	actual := itemMap[op.SectionName(sectionName)][op.FieldName(secretKey)]
+	actual := itemMap[op.SectionName(sectionName)][op.FieldName(itemName)]
 
-	if actual != op.FieldValue(secretValue) {
+	if actual != op.FieldValue(itemValue) {
 		t.Fail()
-		fmt.Printf("Expected to retrieve secretValue. Got '%s' wanted '%s'", actual, secretValue)
+		fmt.Printf("Expected to retrieve item value. Got '%s' wanted '%s'", actual, itemValue)
+	}
+}
+
+func TestSignIn(t *testing.T) {
+	itemName := "itemName"
+	itemValue := "itemValue"
+
+	cli := &FakeCli{
+		ItemName:   itemName,
+		ItemValue: itemValue,
+	}
+
+	domain := "https://externalsecretoperator.1password.com"
+	email := "externalsecretoperator@example.com"
+	secretKey := "AA-BB-CC-DD-EE-FF-GG-HH-II-JJ"
+	masterPassword := "MasterPassword12346!"
+
+	err := cli.SignIn(domain, email, secretKey, masterPassword)
+	if err != nil {
+		t.Fail()
+		fmt.Println("signin should be successful")
 	}
 }
