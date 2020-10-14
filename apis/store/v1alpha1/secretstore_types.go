50e6d62fe0bc71adc64e95cb337910994fce2821
Eric Caleb <caleberic@tuta.io>
2020-10-14 17:27:47 +0200

    use runtime.RawExtension for store

--- apis/store/v1alpha1/secretstore_types.go
+++ apis/store/v1alpha1/secretstore_types.go
@@ -18,6 +18,7 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -27,8 +28,8 @@ import (
 type SecretStoreSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-
-	Store map[string]interface{} `json:"store,omitempty"`
+	Controller string               `json:"controller,omitempty"`
+	Store      runtime.RawExtension `json:"store,omitempty"`
 }
 
 // StoreCondition defines current condition of a Store
