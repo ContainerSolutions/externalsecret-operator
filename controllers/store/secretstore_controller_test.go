7f1c46968f02899dece25e14d322608b0e44b7f7
Eric Caleb <caleberic@tuta.io>
2020-10-14 17:29:06 +0200

    add test for secret store contorller

---
+++ controllers/store/secretstore_controller_test.go
@@ -0,0 +1,10 @@
+package controllers
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("SecretstoreController", func() {
+	Expect("WIP").To(ContainSubstring("WIP"))
+})
