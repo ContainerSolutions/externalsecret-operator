cafd704ff21ed5b731c55b49961e26a33fc0b5c6
Eric Caleb <caleberic@tuta.io>
2020-10-14 17:36:26 +0200

    initialize with credentials from secretRef

--- controllers/store/secretstore_controller.go
+++ controllers/store/secretstore_controller.go
@@ -19,12 +19,19 @@ package controllers
 import (
 	"context"
 
+	storev1alpha1 "github.com/containersolutions/externalsecret-operator/apis/store/v1alpha1"
 	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	storev1alpha1 "github.com/containersolutions/externalsecret-operator/apis/store/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/containersolutions/externalsecret-operator/pkg/backend"
+	config "github.com/containersolutions/externalsecret-operator/pkg/config"
 )
 
 // SecretStoreReconciler reconciles a SecretStore object
@@ -36,12 +43,60 @@ type SecretStoreReconciler struct {
 
 // +kubebuilder:rbac:groups=store.externalsecret-operator.container-solutions.com,resources=secretstores,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=store.externalsecret-operator.container-solutions.com,resources=secretstores/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
 func (r *SecretStoreReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("secretstore", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("secretstore", req.NamespacedName)
 
-	// your logic here
+	// Fetch the ExternalSecret instance
+	secretStore := &storev1alpha1.SecretStore{}
+	err := r.Get(ctx, req.NamespacedName, secretStore)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("SecretStore not found")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get SecretStore")
+		return ctrl.Result{}, err
+	}
+
+	contrl := secretStore.Spec.Controller
+
+	storeConfig := secretStore.Spec.Store.Raw
+
+	config, err := config.ConfigFromCtrl(storeConfig)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	secretRef := config.Auth["secretRef"].(map[string]interface{})
+
+	// Check if this credential Secret exists
+	credentialsSecret := &corev1.Secret{}
+	err = r.Get(ctx, types.NamespacedName{Name: secretRef["name"].(string), Namespace: secretRef["namespace"].(string)}, credentialsSecret)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to get credentials Secret")
+		return ctrl.Result{}, err
+	}
+
+	credentials := credentialsSecret.Data["operator-config.json"]
+
+	err = backend.InitFromCtrl(contrl, config, credentials)
+	if err != nil {
+		log.Error(err, "Failed to intialize backend")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
