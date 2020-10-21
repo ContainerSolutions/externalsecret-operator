0a27a49e9a5c6a45555564984ad76ccfb2ced69b
Eric Caleb <caleberic@tuta.io>
2020-10-21 15:35:40 +0200

    utils: update utils test

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -12,17 +12,43 @@ var _ = Describe("Utils", func() {
 	BeforeEach(func() {})
 
 	AfterEach(func() {})
-	Context("Should generate random string", func() {
+	Context("Should generate random base64 encoded string", func() {
 		It("Should succeed", func() {
-			Expect(len(utils.RandomString(40))).Should(BeEquivalentTo(40))
+			_, err := utils.RandomStringObjectSafe(40)
+			Expect(err).To(BeNil())
 		})
 
 		It("Should not be nil", func() {
-			actual := utils.RandomString(40)
-			Expect(actual).ToNot(BeNil())
+			str, err := utils.RandomStringObjectSafe(40)
+			Expect(err).To(BeNil())
+			Expect(str).ToNot(BeNil())
+		})
+	})
+
+	Context("Should generate random bytes", func() {
+		It("Should succeed", func() {
+			_, err := utils.RandomBytes(40)
+			Expect(err).To(BeNil())
 		})
 
-		// Expect(actual).To(B)
+		It("Should not be nil", func() {
+			str, err := utils.RandomBytes(40)
+			Expect(err).To(BeNil())
+			Expect(str).ToNot(BeNil())
+		})
+	})
+
+	Context("Should generate int64", func() {
+		It("Should succeed", func() {
+			_, err := utils.RandomInt()
+			Expect(err).To(BeNil())
+		})
+
+		It("Should not be nil", func() {
+			ranInt64, err := utils.RandomInt()
+			Expect(err).To(BeNil())
+			Expect(ranInt64).ToNot(BeNil())
+		})
 	})
 
 })
