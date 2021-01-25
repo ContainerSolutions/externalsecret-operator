217c587d0e9af9d5a6c41313718016b5132f3503
Lucas Severo Alves <lucassalves65@gmail.com>
2021-01-25 14:59:05 +0100

    Add schema to crds

---
+++ apis/store/secretstore.go
@@ -0,0 +1,19 @@
+package secretstore
+
+import (
+	"github.com/containersolutions/externalsecret-operator/apis/store/v1alpha1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+}
+
+// AddToSchemes may be used to add all resources defined in the project to a Scheme
+var AddToSchemes runtime.SchemeBuilder
+
+// AddToScheme adds all Resources to the Scheme
+func AddToScheme(s *runtime.Scheme) error {
+	return AddToSchemes.AddToScheme(s)
+}
